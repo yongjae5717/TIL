aa76637c0c6ce466b920b3fca99618a26f82964d
yongjae5717 <dydwo5717@naver.com>
2022-10-02 22:15:51 +0900

    시저암호

--- Programers/level1/시저 암호.py
+++ Programers/level1/시저 암호.py
@@ -12,3 +12,19 @@ def solution(s, n):
                 idx -= 26
         result += chr(idx)
     return result
+
+
+def solution2(s, n):
+    result = ''
+    for i in s:
+        idx = ord (i)
+        if 'a' <= i <= 'z':
+            idx += n
+            if chr(idx) > 'z':
+                idx -= 26
+        elif 'A' <= i <= 'Z':
+            idx += n
+            if chr(idx) > 'Z':
+                idx -= 26
+        result += chr(idx)
+    return result
