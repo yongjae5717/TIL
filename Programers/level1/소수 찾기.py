9fc01165ad98406a06603c71b88ecab3539b177d
yongjae5717 <dydwo5717@naver.com>
2022-10-04 02:03:38 +0900

    에라토스테네스의 체 사용 + solution2: 에라토스테네스의 채를 set을 이용하여 사용

---
+++ Programers/level1/소수 찾기.py
@@ -0,0 +1,16 @@
+def solution(n):
+    lst = [True] * (n + 1)
+    lst[0], lst[1] = False, False  # 1은 소수가 아니다
+
+    m = int (n ** 0.5)
+    for i in range (2, m + 1):
+        if lst[i]:
+            for j in range (2 * i, n + 1, i):
+                lst[j] = False
+
+    # print(lst)
+    count = 0
+    for i in range (2, n + 1):
+        if lst[i]:
+            count += 1
+    return count
