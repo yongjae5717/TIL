4d377440c088a67c17da35e50297f9fec68fec14
yongjae5717 <dydwo5717@naver.com>
2022-10-11 20:12:53 +0900

    20221011 TIL

---
+++ Programers/level2/주식가격.py
@@ -0,0 +1,11 @@
+def solution(prices):
+    answer = []
+    for i in range(len(prices)):
+        price = prices[i]
+        time = 0
+        for j in range(i+1, len(prices)):
+            time += 1
+            if price > prices[j]:
+                break
+        answer.append(time)
+    return answer
