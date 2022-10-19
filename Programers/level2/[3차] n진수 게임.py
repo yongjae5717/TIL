96b66e2fedee52a4533a7ac97043d03b0e12d45a
yongjae5717 <dydwo5717@naver.com>
2022-10-19 15:44:02 +0900

    20221018 TIL

--- Programers/level2/[3차] n진수 게임.py
+++ Programers/level2/[3차] n진수 게임.py
@@ -1,5 +1,28 @@
+"""
+n: 진법
+t: 미리 구할 숫자의 갯수
+m: 게임에 참가하는 인원
+p: 튜브의 순서
+"""
+
+
+def function(n, base):
+    sixteen_number = "0123456789ABCDEF"
+    q, r = divmod(n, base)
+    if q == 0:
+        return sixteen_number[r]
+    else:
+        return function(q, base) + sixteen_number[r]
+
+
 def solution(n, t, m, p):
-    answer = ''
+    answer, temp = '', ''
+    for i in range(m*t):
+        temp += str(function(i, n))
+
+    while len(answer) < t:
+        answer += temp[p-1]
+        p += m
     return answer
 
 
