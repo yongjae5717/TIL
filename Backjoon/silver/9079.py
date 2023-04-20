0ebaec985d0e86ef89eaa6e3c66abb30accd85c0
yongjae <dydwo5717@naver.com>
2023-04-20 10:55:02 +0900

    20230420 TIL (9079 proceeding)

---
+++ Backjoon/silver/9079.py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.readline
+
+
+def main():
+    t = int(input())
+    for _ in range(t):
+        tc = list(list(map(str, input().split())) for _ in range(3))
+
+
+
+
+main()
